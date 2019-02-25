468b327931416c506f9ab33c3bf95225bbbe4e50
Elizabeth Sham <esham00@stuy.edu>
2019-02-25 11:20:38 -0500

    commented out main

--- QueenBoard.java
+++ QueenBoard.java
@@ -157,15 +157,36 @@ public class QueenBoard {
 	    }
 	}
         return countSolutionsH(0);
-    }	
-    public static void main(String[] args) {
-	QueenBoard a = new QueenBoard(4);
-	QueenBoard b = new QueenBoard(8);
-	 // addQueen(0,0);
-	 // addQueen(2,1);
-	 // removeQueen(2,1);
-        a.solve();
-	System.out.println(a.toString());
-        System.out.println(b.countSolutions());
     }
+    //testcase must be a valid index of your input/output array
+    public static void runTest(int i){
+	QueenBoard b;
+	int[]tests =   {1,2,3,4,5,8};
+	int[]answers = {1,0,0,2,10,92};
+	if(i >= 0 && i < tests.length ){
+	    int size = tests[i];
+	    int correct = answers[i];
+	    b = new QueenBoard(size);
+	    int ans  = b.countSolutions();
+   
+	    if(correct==ans){
+		System.out.println("PASS board size: "+tests[i]+" "+ans);
+	    }else{
+		System.out.println("FAIL board size: "+tests[i]+" "+ans+" vs "+correct);
+	    }
+	}
+    }
+    // public static void main(String[] args) {
+    // 	// QueenBoard a = new QueenBoard(4);
+    // 	// QueenBoard b = new QueenBoard(8);
+    // 	//  // addQueen(0,0);
+    // 	//  // addQueen(2,1);
+    // 	//  // removeQueen(2,1);
+    //     // a.solve();
+    // 	// System.out.println(a.toString());
+    //     // System.out.println(b.countSolutions());
+    // 	for (int i = 0; i < 6; i++) {
+    // 	    runTest(i);
+    // 	}
+    // }
 }
